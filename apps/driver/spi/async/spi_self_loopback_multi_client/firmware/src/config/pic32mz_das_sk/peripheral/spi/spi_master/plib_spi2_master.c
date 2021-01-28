71c7af2391481489c8fabc6d65577fc319ecb823
Amit Raddi <amit.raddi@microchip.com>
2021-01-28 19:11:11 +0530

    [DRV_SPI] Regenerated demo for driver_spi_async_spi_self_loopback_multi_client_pic32mz_das_sk
    MH3-49884
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/pic32mz_das_sk/peripheral/spi/spi_master/plib_spi2_master.c
+++ apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/pic32mz_das_sk/peripheral/spi/spi_master/plib_spi2_master.c
@@ -56,7 +56,7 @@ SPI_OBJECT spi2Obj;
 #define SPI2_CON_MODE_32_MODE_16            (0 << _SPI2CON_MODE16_POSITION)
 #define SPI2_CON_ENHBUF                     (1 << _SPI2CON_ENHBUF_POSITION)
 #define SPI2_CON_MCLKSEL                    (0 << _SPI2CON_MCLKSEL_POSITION)
-#define SPI2_CON_MSSEN                      (0 << _SPI2CON_MSSEN_POSITION)
+#define SPI2_CON_MSSEN                      (1 << _SPI2CON_MSSEN_POSITION)
 #define SPI2_CON_SMP                        (0 << _SPI2CON_SMP_POSITION)
 
 void SPI2_Initialize ( void )
@@ -92,7 +92,7 @@ void SPI2_Initialize ( void )
     CKE = 1
     MODE<32,16> = 0
     ENHBUF = 1
-    MSSEN = 0
+    MSSEN = 1
     MCLKSEL = 0
     */
     SPI2CONSET = (SPI2_CON_MSSEN | SPI2_CON_MCLKSEL | SPI2_CON_ENHBUF | SPI2_CON_MODE_32_MODE_16 | SPI2_CON_CKE | SPI2_CON_CKP | SPI2_CON_MSTEN | SPI2_CON_SMP);

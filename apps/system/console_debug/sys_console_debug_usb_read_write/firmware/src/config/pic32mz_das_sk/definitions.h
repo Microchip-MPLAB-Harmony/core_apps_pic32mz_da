2145b97b3d93b35a4bb074f92261929c6ab50963
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-05-17 19:34:36 +0530

    [SYS_CONSOLE] Regenerated demo for system_console_debug_sys_console_debug_usb_read_write_pic32mz_das_sk
    MH3-76216

--- apps/system/console_debug/sys_console_debug_usb_read_write/firmware/src/config/pic32mz_das_sk/definitions.h
+++ apps/system/console_debug/sys_console_debug_usb_read_write/firmware/src/config/pic32mz_das_sk/definitions.h
@@ -56,8 +56,8 @@
 #include "system/time/sys_time.h"
 #include "usb/usb_device_cdc.h"
 #include "usb/usb_cdc.h"
-#include "peripheral/coretimer/plib_coretimer.h"
 #include "driver/usb/usbhs/drv_usbhs.h"
+#include "peripheral/coretimer/plib_coretimer.h"
 #include "bsp/bsp.h"
 #include "system/console/sys_console.h"
 #include "system/console/src/sys_console_usb_cdc_definitions.h"
@@ -75,6 +75,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "PIC32MZ2064DAS169"
+#define DEVICE_ARCH			 "MIPS"
+#define DEVICE_FAMILY		 "PIC32MZDA"
+#define DEVICE_SERIES		 "PIC32MZ"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 200000000
@@ -191,7 +197,7 @@ typedef struct
 {
     SYS_MODULE_OBJ  sysConsole1;
 
-	SYS_MODULE_OBJ  usbDevObject0;
+    SYS_MODULE_OBJ  usbDevObject0;
 
     SYS_MODULE_OBJ  sysTime;
     SYS_MODULE_OBJ  sysDebug;

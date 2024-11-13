6050f495e6ef688ebd93581e68a43bf1d960ed7d
Kevin Abishek S <kevinabishek.s@microchip.com>
2024-11-13 12:02:14 +0530

    [SYS_FS] Regenerated demo for fs__sdmmc_fat__firmware__pic32mz_daa_sk.X
    MH3-103348

--- apps/fs/sdmmc_fat/firmware/src/config/pic32mz_daa_sk/definitions.h
+++ apps/fs/sdmmc_fat/firmware/src/config/pic32mz_daa_sk/definitions.h
@@ -62,11 +62,11 @@
 #include "peripheral/cache/plib_cache.h"
 #include "peripheral/evic/plib_evic.h"
 #include "bsp/bsp.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
@@ -80,13 +80,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32MZ2064DAA288"
-#define DEVICE_ARCH			 "MIPS"
-#define DEVICE_FAMILY		 "PIC32MZDA"
-#define DEVICE_SERIES		 "PIC32MZ"
+#define DEVICE_NAME          "PIC32MZ2064DAA288"
+#define DEVICE_ARCH          "MIPS"
+#define DEVICE_FAMILY        "PIC32MZDA"
+#define DEVICE_SERIES        "PIC32MZ"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 200000000
+#define CPU_CLOCK_FREQUENCY 200000000U
 
 // *****************************************************************************
 // *****************************************************************************
